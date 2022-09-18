9af9d97040af4bc6fceda6fb552f28b0a0de73e8
Anne Castrillon <anne.castrillon@gmail.com>
2022-09-17 22:41:18 -0700

    add light validation for saving a post

--- app/controllers/api/v1/users/dashboard_controller.rb
+++ app/controllers/api/v1/users/dashboard_controller.rb
@@ -1,3 +1,4 @@
+C_POST_BODY_EMPTY=101
 C_NOT_CONFIRMED = 100
 C_NOT_FOUND = 404
 class Api::V1::Users::DashboardController < ApplicationController
@@ -13,16 +14,23 @@ class Api::V1::Users::DashboardController < ApplicationController
   end
 
   def create_post
-    post = Post.create(
-      body:  html_escape(params[:body]),
-      user_id: @user.id,
-      repost_id: params[:repost_id],
-      images: params[:images]
-    )
+    unless params[:body].strip.empty?
+      post = Post.create(
+        body:  html_escape(params[:body]),
+        user_id: @user.id,
+        repost_id: params[:repost_id],
+        images: params[:images]
+      )
 
-    render json: {
-      post: post.render()
-    }
+      render json: {
+        post: post.render()
+      }
+    else
+      render json: {
+        message: 'post body is empty',
+        code: C_POST_BODY_EMPTY
+      }, status: 400
+    end
 
   end
 

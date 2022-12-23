523265fa44c1f407d8b2b3e4610c29f92c4f502d
Anne Castrillon <anne.castrillon@gmail.com>
2022-12-23 15:15:26 -0800

    minor change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.default_url_options = { host: 'https://blogger3-service.onrender.com' }
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
